8e9b3950a2664a063e96fcb744db4e3aed273b68
karthik2tudi <karthik1818t@gmail.com>
2024-09-10 08:17:56 +0530

    AGILE:8888: my feature branch

--- src/main/java/com/rt/MySpringBootMvcProjecct1Application.java
+++ src/main/java/com/rt/MySpringBootMvcProjecct1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySpringBootMvcProjecct1Application {
 
 	public static void main(String[] args) {
+		System.out.println("-------------");
 		SpringApplication.run(MySpringBootMvcProjecct1Application.class, args);
 	}
 
